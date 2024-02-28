6be67c1367fde0ba66cca0a47e7d33ee31c1a6eb
Rick Busarow <rickbusarow@gmail.com>
2024-02-28 11:05:56 -0600

    chore(ci): release automation

---
+++ commitlint.config.js
@@ -0,0 +1,25 @@
+'use strict';
+
+module.exports = {
+  extends: ['@commitlint/config-angular'],
+  rules: {
+    'header-max-length': [1, 'always', 100],
+    'type-enum': [
+      2,
+      'always',
+      [
+        'build',
+        'ci',
+        'chore',
+        'docs',
+        'feat',
+        'fix',
+        'perf',
+        'refactor',
+        'revert',
+        'style',
+        'test'
+      ],
+    ],
+  },
+};
